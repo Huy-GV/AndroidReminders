9b1a94280440c1815bd571b72f5b942a81ea562c
basbaybee <emperorhuyv@gmail.com>
2021-10-23 15:26:36 +1100

    added transition animation, removed back arrow on toolbar

--- app/src/main/java/com/reminders/MainActivity.kt
+++ app/src/main/java/com/reminders/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.reminders.reminders.ReadReminderFragment
 import com.reminders.reminders.ReadReminderFragmentDirections
@@ -21,8 +22,12 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        // Set up the action bar for use with the NavController
-//        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.readTopicFragment, R.id.readReminderFragment,
+            R.id.readReminderFragment, R.id.createUpdateReminderFragment))
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
